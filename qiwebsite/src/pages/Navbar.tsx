9235036e335d1aaa0ecf9aad5bd389a514f7fe30
saravargas88 <128425335+saravargas88@users.noreply.github.com>
2025-09-16 21:36:34 -0400

    navbar

--- qiwebsite/src/pages/Navbar.tsx
+++ qiwebsite/src/pages/Navbar.tsx
@@ -21,6 +21,7 @@ export default function NavBar() {
       variant="dark"
       fixed="top"
       className="shadow-sm"
+      backdrop="blur"
     >
       <Container>
         {/* Brand / Logo */}
